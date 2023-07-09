78cfad77a34f58e0ba465ad4df4a102a6485fb9a
Muhammad Farid Gilang Nuswantoro <faridganteng20050@gmail.com>
2023-07-09 23:47:58 +0700

    modify splashscreen

--- lib/view/splashscreen.dart
+++ lib/view/splashscreen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-import 'package:finalexam/register.dart';
+import 'package:finalexam/login.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:page_transition/page_transition.dart';
@@ -33,7 +33,7 @@ class _SplashscreenState extends State<Splashscreen>
     return AnimatedSplashScreen(
       splash: Lottie.asset('assets/logo.json'),
       backgroundColor: Colors.blue,
-      nextScreen: const Register(),
+      nextScreen: const Login(),
       duration: 3000,
       splashTransition: SplashTransition.fadeTransition,
       pageTransitionType: PageTransitionType.fade,
