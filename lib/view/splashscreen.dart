6025d189ffb7452d1b9c2e53120df0725252d3be
Muhammad Farid Gilang Nuswantoro <faridganteng20050@gmail.com>
2023-07-09 23:24:46 +0700

    add splash screen

---
+++ lib/view/splashscreen.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animationController.forward();
+    Timer(const Duration(seconds: 3), () {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Lottie.asset('assets/logo.json'),
+      backgroundColor: Colors.blue,
+      nextScreen: const Splashscreen(),
+      duration: 3000,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.fade,
+      
+    );
+  }
+}
